e6b963395d41be6c1293968701fa410efc5cdb76
Ubuntu <ubuntu@ip-172-31-21-22.ec2.internal>
2022-05-09 23:45:16 +0000

    added code

--- area_of_tri_with_functions.py
+++ area_of_tri_with_functions.py
@@ -5,31 +5,54 @@
 # This program calculates the area of a triangle with functions
 
 
-def calculate_area(base_as_string, height_as_string):
+def percentage(user_level):
     # calculate temperature
 
-    # output
-    try:
-        height = int(height_as_string)
-        base = int(base_as_string)
-        area = (base * height) / 2
-        print("\nThe area is {} m² ".format(area))
-    except Exception:
-        print("\nInavald, input")
-    finally:
-        print("\nDone")
+    # process & output
+    if user_level == "4+":
+        number = 97
+    elif user_level == "4":
+        number = 89
+    elif user_level == "4-":
+        number = 80
+    elif user_level == "3+":
+        number = 78
+    elif user_level == "3":
+        number = 75
+    elif user_level == "3-":
+        number = 70
+    elif user_level == "2+":
+        number = 68
+    elif user_level == "2":
+        number = 64
+    elif user_level == "2-":
+        number = 60
+    elif user_level == "1+":
+        number = 58
+    elif user_level == "1":
+        number = 55
+    elif user_level == "1-":
+        number = 50
+    elif user_level == "R":
+        number == 30
+    else:
+        number == -1
 
+    return number
 
 def main():
     # This function gets input
 
     # input
-    height_as_string = input("Enter the base (m): ")
-    base_as_string = input("\nEnter the height (m): ")
+    user_level = input("Enter your level and it shall be converted to a percentage: ")
+    print("")
 
     # call functions
-    calculate_area(base_as_string, height_as_string)
+    percentages = percentage(user_level)
 
+    # output
+    print("Your percentage is {0} % ".format(percentages))
+    print("\nDone.")
 
 if __name__ == "__main__":
     main()
